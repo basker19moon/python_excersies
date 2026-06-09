b105d8c42e219ebc60b99550775be0792d094666
Bhaskar Darla <basker19moon@gmail.com>
2026-06-09 06:55:09 +0530

    Changes made to patter program

--- pattern_print.py
+++ pattern_print.py
@@ -1,7 +1,8 @@
+name = input("Please Enter the patter to print: ")
 rows = 5
 for i in range(1, rows+1):
     for k in range(rows-i):
-        print(" ", end='')
+        print(' ', end='')
     for j in range(i):
-        print('*', end=' ')
+        print(name, end=' ')
     print()
